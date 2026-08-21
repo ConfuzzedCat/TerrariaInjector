b5793497879d8d32f3f4b3f8a50ff18a2af4b902
neverify <203544938+neverify@users.noreply.github.com>
2026-08-21 15:14:34 +0300

    feat: add finer exception handling for init calls

--- Program.cs
+++ Program.cs
@@ -254,24 +254,25 @@ namespace TerrariaInjector
                     {
                         continue;   
                     }
-                    
-                    try
+
+                    foreach (var methodName in new string[] { "Init", "Initialize" })
                     {
-                        var initMth = type.GetMethod("Init");
-                        if(initMth != null)
+                        try
                         {
-                            initMth.Invoke(new object(), new object[] { });
+                            var initMth = type.GetMethod(methodName)?.Invoke(new object(), new object[] { });
                         }
-                        var initializeMth = type.GetMethod("Initialize");
-                        if(initializeMth != null)
+                        catch (AmbiguousMatchException e)
                         {
-                            initializeMth.Invoke(new object(), new object[] { });
+                            Logger.Error($"Ambiguous match found for method '{methodName}'.", e);
+                        }
+                        catch (TargetException) { }
+                        catch (TargetParameterCountException) { }
+                        catch (Exception e)
+                        {
+                            Logger.Error($"There was an exception trying to run init methods for {file}.", e);
                         }
                     }
-                    catch (Exception e)
-                    {
-                        Logger.Error($"There was an exception trying to run init methods for {file}.", e);
-                    }
+
                     var prePatchMth = type.GetMethod("PrePatch");
                     if(prePatchMth != null)
                     {
